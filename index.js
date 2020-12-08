5a676849e83ca3c3a26d81937418dfa5960172cc
maxjacobzander <maxjacobzander@gmail.com>
2020-12-07 22:48:59 -0600

    commit tree 5065ba863ecb59b2f2361bfcf12d37b29b3931eb

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] * -1)
+    }
+    return newArray
+};
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i])
+    }
+    return newArray
+};
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] * 2)
+    }
+    return newArray
+};
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] ** 2)
+    }
+    return newArray
+};
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let total = startingPoint
+    for (let i = 0; i < sourceArray.length; i++) {
+        total = total + sourceArray[i]
+    }
+    return total
+};
